2129a681c97ca049a910cfc7edff4d56cd5a56dd
Antonin <antonin.pire@gmail.com>
2023-03-21 16:52:51 +0100

    Fix: Build error w/ pg package using commonJS

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,11 +1,11 @@
 import { PG_CONNECTION_STRING } from '$env/static/private';
 import { Kysely, PostgresDialect, sql, type RawBuilder } from 'kysely';
-import { Pool } from 'pg';
+import pg from 'pg';
 import type { DB } from './schema';
 
 export const db = new Kysely<DB>({
 	dialect: new PostgresDialect({
-		pool: new Pool({
+		pool: new pg.Pool({
 			connectionString: PG_CONNECTION_STRING,
 		}),
 	}),
